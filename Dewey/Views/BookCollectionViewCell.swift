a31f7a4cb9e8170ae5d0b3db910ca40d653eb1d1
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-04-25 16:03:05 -0400

    Updates to home page, add bookshelves page

---
+++ Dewey/Views/BookCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookCollectionViewCell.swift
+//  Dewey
+//
+//  Created by Jessica Huynh on 2020-04-25.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bookCover: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var bookAuthor: UILabel!
+}
