e94cc1105e074bf6070187b53fc26264eca34743
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-04-25 10:45:02 -0400

    File reorganization

--- Dewey/ViewControllers/HomeViewController.swift
+++ Dewey/ViewControllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Dewey
 //
 //  Created by Jessica Huynh on 2020-04-25.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
