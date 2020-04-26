56e21c5cb1d37ec681bbda3ca5a4298399abe1bf
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-04-25 20:17:35 -0400

    Add animation to header

---
+++ Dewey/Views/HomeHeaderTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  HomeHeaderTableViewCell.swift
+//  Dewey
+//
+//  Created by Jessica Huynh on 2020-04-25.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderTableViewCell: UITableViewCell {
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        header.alpha = 0
+        UIView.animate(withDuration: 1, delay: 0.2, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.header.alpha = 1
+        }, completion: nil)
+    }
+}
