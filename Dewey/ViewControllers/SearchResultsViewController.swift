a86806e2ff48bf8589432cc32bfe906c6f1fbbb0
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-04-28 18:24:12 -0400

    Add search results page

---
+++ Dewey/ViewControllers/SearchResultsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SearchResultsViewController.swift
+//  Dewey
+//
+//  Created by Jessica Huynh on 2020-04-28.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var searchQuery: String!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.text = searchQuery
+        
+        let bookDetailsCell = UINib(nibName: "BookDetailsTableViewCell", bundle: nil)
+        tableView.register(bookDetailsCell, forCellReuseIdentifier: "BookDetailsCell")
+    }
+    
+    // MARK: - Table View Delegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let bookViewController = self.storyboard?.instantiateViewController(withIdentifier: "BookViewController") as! BookViewController
+        bookViewController.book = StorageManager.instance.bookshelves.first!.books[indexPath.row]
+        navigationController?.pushViewController(bookViewController, animated: true)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookDetailsCell", for: indexPath) as! BookDetailsTableViewCell
+        cell.configure(book: StorageManager.instance.bookshelves.first!.books[indexPath.row])
+        return cell
+    }
+
+}
---
+++ Dewey/Views/BookDetailsTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  BookDetailsTableViewCell.swift
+//  Dewey
+//
+//  Created by Jessica Huynh on 2020-04-28.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailsTableViewCell: UITableViewCell {
+    @IBOutlet weak var bookCover: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var publicationYear: UILabel!
+    @IBOutlet weak var bookDescription: UILabel!
+    
+    func configure(book: Book) {
+        let url = URL(string: book.cover)
+        bookCover.kf.indicatorType = .activity
+        bookCover.kf.setImage(
+            with: url,
+            placeholder: UIImage(named: "book-cover-placeholder"),
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage
+            ])
+        
+        bookTitle.text = book.title
+        author.text = book.author
+        bookDescription.text = book.description
+    }
+}
