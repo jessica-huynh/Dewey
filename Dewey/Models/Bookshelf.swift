18dde7178aa59b63f7543e65b52d628cbd2e43d1
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-04-30 16:59:40 -0400

    Change from struct to class, add some helper functions

--- Dewey/Models/Bookshelf.swift
+++ Dewey/Models/Bookshelf.swift
@@ -8,7 +8,29 @@
 
 import Foundation
 
-struct Bookshelf {
+class Bookshelf {
     var name: String
     var books: [Book] = []
+    
+    init(name: String, books: [Book] = []) {
+        self.name = name
+        self.books = books
+    }
+    
+    func contains(book: Book) -> Bool {
+        return books.contains(where: { $0.isbn == book.isbn })
+    }
+    
+    func addBook(book: Book) {
+        if self.contains(book: book) { return }
+        books = [book] + books // Maintain order by most recently added
+    }
+    
+    func removeBook(at index: Int) {
+        books.remove(at: index)
+    }
+    
+    func removeAllBooks() {
+        books = []
+    }
 }
